e37c4b21d354c11deaf3e9e3798783ef41b85b31
Diana Rusak <dianka23@inbox.ru>
2017-10-13 02:02:47 +1000

    before 6 hour for deadline
    when you code

--- paint1.pas
+++ paint1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-  ExtCtrls, Menus, StdCtrls; //painted;
+  ExtCtrls, Menus, StdCtrls;
 
 type
 
@@ -19,9 +19,16 @@ type
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
     PaintBox1: TPaintBox;
+
     procedure FormCreate(Sender: TObject);
-    procedure FormWhite();
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
+    procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
+    procedure FormMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
     procedure MenuItem4Click(Sender: TObject);
+    procedure OnPaint();
+    procedure PaintBox1Paint(Sender: TObject);
   private
     { private declarations }
   public
@@ -30,6 +37,9 @@ type
 
 var
   Form1: TForm1;
+  DownClick: Boolean; //флаг нажатой кнопки
+  LineralArray: array of array of
+  Tpoint;//во внешнем - линии, во внутреннем - точки линии
 
 implementation
 
@@ -42,16 +52,53 @@ begin
   ShowMessage('Б8103а 2017 год. Русак Диана Игоревна');  //справка
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.PaintBox1Paint(Sender: TObject);
 begin
-  FormWhite();
+  with PaintBox1.Canvas do begin
+    Brush.Color := clWhite;
+    Pen.Width := 1;
+    FillRect(0, 0, Width, Height);
+  end;
 end;
 
-procedure TForm1.FormWhite();
+procedure TForm1.FormCreate(Sender: TObject);
 begin
-  PaintBox1.Canvas.Brush.Color := clWindow;
-  PaintBox1.Canvas.FillRect(PaintBox1.Canvas.ClipRect);
+  DownClick := False;
+  with PaintBox1.Canvas do begin
+    Brush.Color := clWhite;
+    Pen.Width := 1;
+  end;
+end;
+
+procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+begin
+  DownClick := True;
+  SetLength(LineralArray, Length(LineralArray) + 1);
+  SetLength(LineralArray[0], Length(LineralArray[0]) + 1);
+  LineralArray[Length(LineralArray)-1, Length(LineralArray[0])-1] := Point(x, y);
+  Invalidate;
+end;
+
+procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
+begin
+  if DownClick then begin
+    Setlength(LineralArray[length(LineralArray)], length(LineralArray[length(LineralArray)]) + 1);
+    LineralArray[Length(LineralArray) - 1, Length(LineralArray[Length(LineralArray) - 1]) - 1] := Point(x, y);
+  end;
+  Invalidate;
+end;
+
+procedure TForm1.FormMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+begin
+  DownClick := False;
+  Invalidate;
+end;
+
+procedure TForm1.OnPaint();
+begin
+  Paintbox1.canvas.polyline(LineralArray[1]);
 end;
 
 end.
-
